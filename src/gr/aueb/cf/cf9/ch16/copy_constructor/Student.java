76ab091016ffee3bd83678bf5244a806de0112c0
jboutos <215729236+jboutos@users.noreply.github.com>
2025-12-17 15:33:34 +0200

    feat: add exercises

---
+++ src/gr/aueb/cf/cf9/ch16/copy_constructor/Student.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.cf9.ch16.copy_constructor;
+
+public class Student {
+    private String name;
+    private City city;
+
+    public Student() {
+    }
+
+    public Student(String name, City city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public Student(Student student) {
+        this.name = student.name;
+        //this.city = student.city; //lathos odigei se shallow copy
+        this.city = new City(student.getCity());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public City getCity() {
+        //return city; to idio edo
+        return new City(this.city);
+    }
+
+    public void setCity(City city) {
+        //this.city = city;
+        this.city = new City(city);
+    }
+}
